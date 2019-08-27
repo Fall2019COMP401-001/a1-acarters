9cc3a34802daac9827d16ed6d79a7ee7a8a12cbf
acarters <acarters@acarters-pc.wireless-1x.unc.edu>
2019-08-26 20:08:54 -0400

    My first attempt at the Adept program.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,65 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int itemNum = scan.nextInt();
+		String[] nameArr = new String[itemNum];
+		double[] priceArr = new double[itemNum];
+		for(int i = 0; i < itemNum; i++)
+		{
+		String itemName = scan.next();
+		double itemPrice = scan.nextDouble();
+		nameArr[i] = itemName;
+		priceArr[i] = itemPrice;
+		}
+		int customerNum = scan.nextInt();
+		String largeFirst = "";
+		String largeLast = "";
+		String smallFirst = "";
+		String smallLast = "";
+		double largeTotal = 0.0;
+		double smallTotal = 9999999.99;
+		double grandTotal = 0.0;
+		
+		for(int i = 0; i < customerNum; i++)
+		{
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int itemCount = scan.nextInt();
+			double total = 0.0;
+			for (int j = 0; j < itemCount; j++)
+			{
+				int quant = scan.nextInt();
+				String name = scan.next();
+				for (int k = 0; k < priceArr.length; k++)
+				{
+				if (name == nameArr[k])
+				{
+					total += quant * priceArr[k];
+				}
+				}
+		}
+			grandTotal += total;
+			if (total >= largeTotal)
+			{
+				largeTotal = total;
+				largeFirst = firstName;
+				largeLast = lastName;
+			}
+			if (total <= smallTotal)
+			{
+				smallTotal = total;
+				smallFirst = firstName;
+				smallLast = lastName;
+			}
+		}
+		double average = grandTotal / customerNum;
+		String largeStr = String.format("%.2f", largeTotal);
+		String smallStr = String.format("%.2f", smallTotal);
+		String avgStr = String.format("%.2f", average);
+		System.out.println("Biggest: " + largeFirst + " " + largeLast + " (" + largeStr + ")");
+		System.out.println("Smallest: " + smallFirst + " " + smallLast + " (" + smallStr + ")");
+		System.out.println("Average: " + avgStr);
+		scan.close();
 		
 	}
 }
